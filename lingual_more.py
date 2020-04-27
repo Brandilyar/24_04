88c3956e2b62585111c5032e864cbf6b467311db
Brandilyar <63740901+Brandilyar@users.noreply.github.com>
2020-04-27 13:46:13 +0300

    Update lingual_more.py

--- lingual_more.py
+++ lingual_more.py
@@ -1,3 +1,4 @@
+import random
 print(" Cейчас вам будет предоставлена возможность введения слов")
 print(" Когда заходите остановиться, просто введите q")
 
@@ -9,22 +10,23 @@ while True:
         break
     value = input("Ведите слово на русском\n: ").strip().lower()
     slovar[key] = value
-print(slovar)
 
 print("Сейчас у нас будет проверка, больше 3 ошибок нельзя")
 
 errors = 0
 bonus = 0
 
-for key in slovar.keys():
-    print("Ввeдите перевод слова", key, ": ")
-    answer = input(": ").strip().lower()
-    if slovar[key] == answer:
-        bonus += 1
-        print("Ваш счет составляет", bonus)
-    elif errors > 3:
-        print("game over")
-        break
-    else:
-        errors +=1
-        print(slovar[key], "- правильный ответ")
+while len(slovar) != 0:
+	item = random.choice(list(slovar.keys()))	
+	print("Ввeдите перевод слова", item, ": ")
+	answer = input(": ").strip().lower()
+	if slovar[item] == answer:
+		bonus += 1
+		print("Ваш счет составляет", bonus)
+	elif errors > 3:
+		print("game over")
+		break
+	else:
+		errors +=1
+		print(slovar[item], "- правильный ответ")
+	slovar.pop(item)
